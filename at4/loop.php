f9ebd83cc73506595cf45ab62594da388f7efea6
matheus15-hub <matheus.omega15@gmail.com>
2026-07-28 00:13:03 -0300

    atividade 4: loop de 1 a 100

---
+++ at4/loop.php
@@ -0,0 +1,8 @@
+<?php 
+for($i = 1 ; $i < 100 ; $i++ ){
+    if($i %2 == 0){
+        echo "<br>". $i;
+    }
+}
+?>
+//Professor para ver no navegador pois no terminal não esta aparecendo a opção de eu rodar o codigo
