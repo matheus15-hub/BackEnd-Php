57bc15c175a0908dac3347697cd54aced203f5b9
matheus15-hub <matheus.omega15@gmail.com>
2026-07-28 00:18:36 -0300

    atividade 5 : tabuadas

---
+++ at5/tabuadas.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "<h3>Tabuada do 4</h3>";
+for($i = 1; $i <= 10; $i++){
+    echo "4 x $i = " . (4 * $i) . "<br>";
+}
+
+echo "<br><h3>Tabuada do 7</h3>";
+for($i = 1; $i <= 10; $i++){
+    echo "7 x $i = " . (7 * $i) . "<br>";
+}
+
+echo "<br><h3>Tabuada do 12.879,5</h3>";
+for($i = 1; $i <= 10; $i++){
+    echo "12.879,5 x $i = " . (12879.5 * $i) . "<br>";
+}
+
+?>
