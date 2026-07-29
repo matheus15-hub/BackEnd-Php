0ac7ddd709f102978f1a2c1e50defc4dd2cd9f56
matheus15-hub <matheus.omega15@gmail.com>
2026-07-29 20:43:04 -0300

    atividade 8 : foreach

---
+++ at8/memes.php
@@ -0,0 +1,13 @@
+<?php 
+    $memes = [
+    "Luva de Pedreiro",
+    "Caneta Azul",
+    "Nazaré Confusa",
+    "É Verdade eu não minto ",
+    "Taca-le Pau, Marcos",
+    ];
+
+    foreach($memes as $m){
+        echo $m ."<br>";
+    }   
+    ?>
